0ab90ca26230b3ff22346ad4d84e342fdc0bf313
Scott Dawson <github@dawsonmediadesign.com>
2025-12-08 07:08:17 -0500

    fix

--- _data/pottery.js
+++ _data/pottery.js
@@ -1,42 +1,42 @@
-const axios = require("axios").default;
-require("dotenv").config();
-let ETSY_KEY = process.env.ETSY_KEY;
+// const axios = require("axios").default;
+// require("dotenv").config();
+// let ETSY_KEY = process.env.ETSY_KEY;
 
-module.exports = async function () {
-  let pottery = [];
-  let shopId = 24030861;
-  let limit = 9;
-  let config = {
-    headers: {
-      "x-api-key": `${ETSY_KEY}`,
-    },
-  };
+// module.exports = async function () {
+//   let pottery = [];
+//   let shopId = 24030861;
+//   let limit = 9;
+//   let config = {
+//     headers: {
+//       "x-api-key": `${ETSY_KEY}`,
+//     },
+//   };
 
-  await axios
-    .get(
-      `https://openapi.etsy.com/v3/application/shops/${shopId}/listings/featured`,
-      config
-    )
-    // await axios.get(`https://openapi.etsy.com/v2/shops/${shopId}/listings/active?limit=${limit}&taxonomy_id=6096&api_key=${ETSY_KEY}`)
-    .then((result) => {
-      pottery = result.data.results;
-    })
-    .catch((err) => console.log(err));
+//   await axios
+//     .get(
+//       `https://openapi.etsy.com/v3/application/shops/${shopId}/listings/featured`,
+//       config
+//     )
+//     // await axios.get(`https://openapi.etsy.com/v2/shops/${shopId}/listings/active?limit=${limit}&taxonomy_id=6096&api_key=${ETSY_KEY}`)
+//     .then((result) => {
+//       pottery = result.data.results;
+//     })
+//     .catch((err) => console.log(err));
 
-  // get the images for each of the pottery pieces
-  for (let i = 0; i < pottery.length; i++) {
-    await axios
-      .get(
-        `https://openapi.etsy.com/v3/application/listings/${pottery[i].listing_id}/images`,
-        config
-      )
-      .then((result) => {
-        pottery[i].image_url = result.data.results[0].url_570xN;
-        pottery[i].px = pottery[i].price.amount / pottery[i].price.divisor;
-      });
-  }
-  return []; //pottery.slice(0, limit);
-};
+//   // get the images for each of the pottery pieces
+//   for (let i = 0; i < pottery.length; i++) {
+//     await axios
+//       .get(
+//         `https://openapi.etsy.com/v3/application/listings/${pottery[i].listing_id}/images`,
+//         config
+//       )
+//       .then((result) => {
+//         pottery[i].image_url = result.data.results[0].url_570xN;
+//         pottery[i].px = pottery[i].price.amount / pottery[i].price.divisor;
+//       });
+//   }
+//   return pottery.slice(0, limit);
+// };
 
 // [ { listing_id: 1248883301,
 //     user_id: 328045470,
